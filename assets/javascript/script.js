13608c7e41b0cd0987eedb3896d18c9f9f5072e8
IsraelMuCa <isra.munoz@gmail.com>
2018-05-28 23:51:58 -0500

    Finished the basic UI with JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function (){
+    /*
+    DEPRECATED THE ONE PAGE APP, USING SEVERAL PAGES NOW
     //Catch containers
     var createNoteContainer = $("#create-note-container");
     var searchNoteContainer = $("#search-notes-container");
@@ -24,5 +26,43 @@ $(document).ready(function (){
             searchNoteContainer.show();
         }
     });
+    */
+
+    //Global variables
+    var minimizeCustData = false;
+    var minimizeEquipData = false;
+    var minimizeRecentNotes = false;
+
+
+    //Click listeners for hiding containers as clicks are done on the respective boxes
+    $('.minimize-cust-data-click').on('click', function() {
+        if (!minimizeCustData) {
+            $('.minimize-cust-data').hide();
+            minimizeCustData = true;
+        } else {
+            $('.minimize-cust-data').show();
+            minimizeCustData = false;
+        }
+    });
+    $('.minimize-equipment-data-click').on('click', function() {
+        if (!minimizeEquipData) {
+            $('.minimize-equipment-data').hide();
+            minimizeEquipData = true;
+        } else {
+            $('.minimize-equipment-data').show();
+            minimizeEquipData = false;
+        }
+    });
+
+    $('.minimize-recent-notes-click').on('click', function() {
+        if (!minimizeRecentNotes) {
+            $('.minimize-recent-notes').hide();
+            minimizeRecentNotes = true;
+        } else {
+            $('.minimize-recent-notes').show();
+            minimizeRecentNotes = false;
+        }
+    });
+
 
 });
