f3914068b8fdb5fc89fcc3281c3c00354d3bed31
IsraelMuCa <isra.munoz@gmail.com>
2018-05-29 12:21:50 -0500

    Started trying to separate the JS files to make the app lighter in each section, realized will have to create the ticket as an Object with functions

--- assets/javascript/result.js
+++ assets/javascript/result.js
@@ -1,32 +1,4 @@
 $(document).ready(function (){
-    /*
-    DEPRECATED THE ONE PAGE APP, USING SEVERAL PAGES NOW
-    //Catch containers
-    var createNoteContainer = $("#create-note-container");
-    var searchNoteContainer = $("#search-notes-container");
-
-    //Hide containers
-    createNoteContainer.show();
-    searchNoteContainer.hide();
-
-    //Function to switch tabs
-    $('.tabs li').on('click', function() {
-        var tabClicked = $(this);
-        
-        if (tabClicked[0].id == "create-note-list") {
-            tabClicked.addClass("is-active");
-            $("#search-notes-list").removeClass("is-active");
-            searchNoteContainer.hide();
-            createNoteContainer.show();
-
-        } else if (tabClicked[0].id == "search-notes-list") {
-            tabClicked.addClass("is-active");
-            $("#create-note-list").removeClass("is-active");
-            createNoteContainer.hide();
-            searchNoteContainer.show();
-        }
-    });
-    */
 
     //Global variables
     var minimizeCustData = false;
