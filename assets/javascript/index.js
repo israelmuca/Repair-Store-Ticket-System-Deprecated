9a604dd474936f3ad1bc26b2d68ac75ade1d1c5c
IsraelMuCa <isra.munoz@gmail.com>
2018-06-04 22:08:43 -0500

    Started messing around with Firebase, uploading advances

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,65 @@
 $(document).ready(function (){
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAYkl5opTyW9YF7801KmgT9YUhpV0JhYGY",
+        authDomain: "notas-xtm-fixc.firebaseapp.com",
+        databaseURL: "https://notas-xtm-fixc.firebaseio.com",
+        projectId: "notas-xtm-fixc",
+        storageBucket: "notas-xtm-fixc.appspot.com",
+        messagingSenderId: "768789228246"
+    };
+
+    firebase.initializeApp(config);
+    var database = firebase.database();
     
-    //Global variables
+
+    //GLOBAL VARIABLES
+    //Hide/unhide booleans
     var minimizeCustData = false;
     var minimizeEquipData = false;
 
-    //Global selectors
-    var sucursalSelector = $('.sucursal-select');
+    //ticket data
+    var location;
+    var ticketNum;
+    var date;
+
+    //cust data
+    var custName;
+    var custLastName;
+    var cellNum;
+    var email;
+    var zipCode;
+    var prefContactMet;
+
+    //equipment data
+    var eqType;
+    var eqBrand;
+    var eqModel;
+    var eqSerialNum;
+    var characteristics;
+    var accesories;
+    var reasonToVisit;
+
+
+
+    //GLOBAL SELECTORS
+    var locationSelector = $('#location-select');
+    var ticketNumSelector = $('#ticket-number');
+    var dateSelector = $('#new-ticket-date-now');
+    var custNameSelector = $('#cust-name');
+    var custLastNameSelector = $('#cust-last-name');
+    var cellNumSelector = $('#cellphone-number');
+    var emailSelector = $('#email');
+    var zipCodeSelector = $('#zip-code');
+    var prefContactMetSelector = $('#');
+    var eqTypeSelector = $('#equipment-type');
+    var eqBrandSelector = $('#equipment-brand');
+    var eqModelSelector = $('#equipment-model');
+    var eqSerialNumSelector = $('#equipment-serial-number');
+    var characteristicsSelector = $('#equipment-characteristics');
+    var accesoriesSelector = $('#equipment-accesories');
+    var reasonToVisitSelector = $('#equipment-reason');
 
     //Momentjs date handler
     //Sets the date for the new tickets to now; SAVE BUTTON SHOULD SAVE WITH HOUR
@@ -32,7 +86,7 @@ $(document).ready(function (){
     });
 
     //Change listener to apply the 1st character of the new ticket number
-    sucursalSelector.on('change', function() {
+    locationSelector.on('change', function() {
         var numeroFolio = $('#numero-folio');
         if (this.value == 'Avanta') {
             numeroFolio.val('A');
@@ -45,5 +99,7 @@ $(document).ready(function (){
         //IT SHOULD THEN UNHIDE THE 'DATOS DEL CLIENTE' CONTAINER
         //ONCE THE 'DATOS DEL CLIENTE' CONTAINER IS FILLED, UNHIDE DATOS DEL EQUIPO
     });
+
+
 
 });
