ba97286d3df2f5fb99d170ebad7b02896d4e78c2
IsraelMuCa <isra.munoz@gmail.com>
2018-06-13 13:41:25 -0500

    Fixed the goToSearch function for online functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -439,16 +439,15 @@ $(document).ready(function (){
     function goToSearch() {
         /*
             Take the user to the search page (for local work)
-        */
         var oldURL = window.location.href;
         var newURL = oldURL.replace("index", "search");
         window.location.replace(newURL);
+        */
         
 
         /*
-            Take the user to the search page (for Git online work)
+            Take the user to the search page (for Git online work) */
         window.location.href = 'search.html';
-        */
         
     }
 // --------------------- FUNCTIONS -   END ---------------------
